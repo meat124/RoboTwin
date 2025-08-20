Fix: Action normalization

policy/DiT/data4robotics/replay_buffer.py
@@ -4,6 +4,7 @@
 # LICENSE file in the root directory of this source tree.
 
 
+import json
 import os
 import pickle as pkl
 import random
@@ -90,6 +91,14 @@ class RobobufReplayBuffer(Dataset):
         if os.path.exists(norm_file):
             shutil.copyfile(norm_file, "./ac_norm.json")
 
+            with open("./ac_norm.json", "r") as f:
+                ac_norm = json.load(f)
+                ac_loc = np.array(ac_norm["loc"], dtype=np.float32)
+                ac_scale = np.array(ac_norm["scale"], dtype=np.float32)
+                global _AC_LOC, _AC_SCALE
+                _AC_LOC = torch.tensor(ac_loc, dtype=torch.float32)
+                _AC_SCALE = torch.tensor(ac_scale, dtype=torch.float32)
+
         # shuffle the list with the fixed seed
         rng = random.Random(BUF_SHUFFLE_RNG)
 
@@ -168,4 +177,4 @@ class RobobufReplayBuffer(Dataset):
         assert (
             loss_mask.shape[0] == a_t.shape[0]
         ), "a_t and mask shape must be ac_chunk!"
-        return (i_t, o_t), a_t, loss_mask
+        return (i_t, o_t), a_t, loss_mask, (_AC_LOC, _AC_SCALE)

policy/DiT/data4robotics/task.py
@@ -67,11 +67,16 @@ class BCTask(DefaultTask):
         losses = []
         action_l2, action_lsig = [], []
         for batch in self.test_loader:
-            (imgs, obs), actions, mask = batch
+            (imgs, obs), actions, mask, (ac_loc, ac_scale) = batch
             imgs = {k: v.to(trainer.device_id) for k, v in imgs.items()}
-            obs, actions, mask = [
-                ar.to(trainer.device_id) for ar in (obs, actions, mask)
+            obs, actions, mask, ac_loc, ac_scale = [
+                ar.to(trainer.device_id) for ar in (obs, actions, mask, ac_loc, ac_scale)
             ]
+            ac_loc_expanded, ac_scale_expanded = ac_loc.unsqueeze(1), ac_scale.unsqueeze(1)
+            
+            # normalize actions
+            obs = (obs - ac_loc) / ac_scale
+            actions = (actions - ac_loc_expanded) / ac_scale_expanded
 
             with torch.no_grad():
                 loss = trainer.training_step(batch, global_step)
@@ -109,3 +114,4 @@ class BCTask(DefaultTask):
                 },
                 step=global_step,
             )
+        return mean_val_loss

policy/DiT/deploy_policy.py
@@ -5,16 +5,44 @@ import sys, os
 import yaml
 import cv2
 import numpy as np
+import json
 
 from .dit_model import DiT
+import matplotlib.pyplot as plt
+from collections import deque
+from torchvision import transforms
+
 
 IMAGE_SIZE = (256, 256)  # Fixed image size for resizing
+EXP_WEIGHT = 0.01
+_AC_LOC = None
+_AC_SCALE = None
+
+with open("./ac_norm.json", "r") as f:
+    ac_norm = json.load(f)
+    ac_loc = np.array(ac_norm["loc"], dtype=np.float32)
+    ac_scale = np.array(ac_norm["scale"], dtype=np.float32)
+    _AC_LOC = ac_loc
+    _AC_SCALE = ac_scale
+
+
+def get_preproc_transform(size):
+    return transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Resize((size, size), antialias=False),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+        ),
+    ])
 
 
 def encode_obs(observation):  # Post-Process Observation
     def resize_obs(bgr_img: np.ndarray, size=IMAGE_SIZE):
         resized = cv2.resize(bgr_img, size, interpolation=cv2.INTER_AREA)
-        return torch.tensor(resized, dtype=torch.float32).permute(2, 0, 1).float() / 255.0
+        resized_rgb = resized[:, :, ::-1].copy()  # Convert BGR to RGB
+        # resized = get_preproc_transform(size[0])(resized)
+        # resized = resized.float() / 255.0  # Convert to CxHxW format and normalize
+        return torch.from_numpy(resized_rgb).permute(2, 0, 1).float() / 255.0  # Convert to CxHxW format and normalize
 
     front_cam = resize_obs(observation["observation"]["front_camera"]["rgb"]) if "front_camera" in observation["observation"] else None
     head_cam = resize_obs(observation["observation"]["head_camera"]["rgb"]) if "head_camera" in observation["observation"] else None
@@ -27,10 +55,7 @@ def encode_obs(observation):  # Post-Process Observation
         cam3=right_cam,
     )
     obs["agent_pos"] = torch.tensor(observation["joint_action"]["vector"], dtype=torch.float32)
-    print(f"obs['agent_pos'].shape: {obs['agent_pos'].shape}")  # Debugging
-    print(f"obs['agent_pos'].max(): {obs['agent_pos'].max()}, min: {obs['agent_pos'].min()}")  # Debugging
-    print(f"obs['agent_pos'].mean(): {obs['agent_pos'].mean()}, std: {obs['agent_pos'].std()}")  # Debugging
-    print(f"obs['agent_pos']: {obs['agent_pos']}")  # Debugging
+    obs["agent_pos"] = (obs["agent_pos"] - _AC_LOC) / _AC_SCALE
 
     return obs
 
@@ -46,6 +71,11 @@ def get_model(usr_args):  # from deploy_policy.yml and eval.sh (overrides)
     ac_chunk = model_training_config['ac_chunk']
     img_chunk = model_training_config['img_chunk']
 
+    import shutil
+    norm_file = os.path.join(os.path.dirname(ckpt_file), "ac_norm.json")
+    if os.path.exists(norm_file):
+        shutil.copyfile(norm_file, "./ac_norm.json")
+
     model = DiT(
         ckpt_file=ckpt_file,
         ac_chunk=ac_chunk,
@@ -55,27 +85,61 @@ def get_model(usr_args):  # from deploy_policy.yml and eval.sh (overrides)
     return model
 
 
+act_history = None
+
+
 def eval(TASK_ENV, model, observation):
     """
     All the function interfaces below are just examples
     You can modify them according to your implementation
     But we strongly recommend keeping the code logic unchanged
     """
-    obs = encode_obs(observation)  # Post-Process Observation
+    global act_history
+    obs = encode_obs(observation)
     instruction = TASK_ENV.get_instruction()
+    
+    # ======== Get Action with Temporal Ensemble ========
+    actions = model.get_action(obs)
+    if act_history is None:
+        act_history = deque(maxlen=len(actions))
+    act_history.append(actions)
 
-    actions = model.get_action(obs)  # Get Action according to observation chunk
-    actions = actions[:5]
+    num_actions = len(act_history)
+    curr_act_preds = np.stack(
+        [
+            pred_actions[i]
+            for (i, pred_actions) in zip(
+                range(num_actions - 1, -1, -1), act_history
+            )
+        ]
+    )
 
-    for action in actions:  # Execute each step of the action
-        # see for https://robotwin-platform.github.io/doc/control-robot.md more details
-        TASK_ENV.take_action(action) # joint control: [left_arm_joints + left_gripper + right_arm_joints + right_gripper]
-        # TASK_ENV.take_action(action, action_type='ee') # endpose control: [left_end_effector_pose (xyz + quaternion) + left_gripper + right_end_effector_pose + right_gripper]
-        observation = TASK_ENV.get_obs()
-        obs = encode_obs(observation)
-        model.update_obs(obs)  # Update Observation, `update_obs` here can be modified
+    # compute the weighted average across all predictions for this timestep
+    weights = np.exp(-EXP_WEIGHT * np.arange(num_actions))[::-1]
+    weights = weights / weights.sum()
+    action = np.sum(weights[:, None] * curr_act_preds, axis=0)  # weighted average
+
+    action = action * _AC_SCALE + _AC_LOC  # denormalize the action
+
+    TASK_ENV.take_action(action)
+    observation = TASK_ENV.get_obs()
+    obs = encode_obs(observation)
+    model.update_obs(obs)
+
+
+    # # ======== Get Action without Temporal Ensemble ========
+    # actions = model.get_action(obs)
+    
+    # for action in actions:
+    #     action = action * _AC_SCALE + _AC_LOC
+    #     TASK_ENV.take_action(action)
+    #     observation = TASK_ENV.get_obs()
+    #     obs = encode_obs(observation)
+    #     model.update_obs(obs)
 
 
 def reset_model(model):  
+    global act_history
     # Clean the model cache at the beginning of every evaluation episode, such as the observation window
     model.reset_obs()
+    act_history = None